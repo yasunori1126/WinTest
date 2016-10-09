4d56d7763d9ad27d52bcc8527f9b56e856132eca
Your Name <you@example.com>
2016-10-09 23:39:46 +0900

    コミット１

--- WinTest/Form1.cs
+++ WinTest/Form1.cs
@@ -16,5 +16,10 @@ namespace WinTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("TEST");
+        }
     }
 }
